189933e4bf95366be26c2ce6e713eeacf08910fe
Vedika Jain <vedikajain.350@gmail.com>
2017-08-26 00:01:39 +0200

    creating politicans

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def memory
+    @politicans = Politician.all
+
+    @cards = []
+    @politicans.each do |p|
+      @cards << p.name
+      @cards << p.party
+    end
+
+    @cards.shuffle!
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+politicans_attributes = [
+              { name: 'Angela Merkel', party:"CDU" },
+              { name: 'Martin Schulz', party:"SPD" },
+              { name: 'Sigmar Gabriel', party:"SPD" },
+              { name: 'Frank-Walter Steinmeier', party:"SPD" },
+              { name: 'Wolfgang Schäuble', party:"CDU" },
+              { name: 'Helmut Kohl', party:"CDU" },
+              { name: 'Willy Brandt', party:"SPD" },
+              { name: 'Horst Seehofer', party:"CSU" },
+              ]
+
+politicans_attributes.each do |p|
+  Politician.create!(p)
+end
