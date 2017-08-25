94af6de3c085c726dcb0200ca5433e692e90e276
Vedika Jain <vedikajain.350@gmail.com>
2017-08-26 00:06:45 +0200

    home change

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825203122) do
+ActiveRecord::Schema.define(version: 20170825214159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-
-  create_table "politicians", force: :cascade do |t|
-    t.string "name"
-    t.string "party"
-  end
 
   create_table "answers", force: :cascade do |t|
     t.string "text"
@@ -28,6 +22,13 @@ ActiveRecord::Schema.define(version: 20170825203122) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "politicians", force: :cascade do |t|
+    t.string "name"
+    t.string "party"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
